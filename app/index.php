253ae165d1429d044e111c1371ce2dac2eb39ed7
Vinicius Vibrich <44097918+vvibrich@users.noreply.github.com>
2019-01-22 13:10:31 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,22 +1,18 @@
 <?php
-
 $dbuser = $_ENV['MYSQL_USER'];
 $dbpass = $_ENV['MYSQL_PASS'];
-
 try {
-    $pdo = new PDO("mysql:host=mysql;dbname=compose", $dbuser, $dbpass);
-    $statement = $pdo->prepare("SELECT * FROM hello");
+    $pdo = new PDO("mysql:host=mysql;dbname=blog", $dbuser, $dbpass);
+    $statement = $pdo->prepare("SELECT * FROM posts");
     $statement->execute();
     $posts = $statement->fetchAll(PDO::FETCH_OBJ);
     
-    echo "<h2>Hello World Compose!</h2>";
+   echo "<h2>Hello World Compose!</h2>";
     echo "<ul>";
-    foreach ($hello as $hello ) {
-        echo "<li>".$hello->title."</li>";
+    foreach ($posts as $post ) {
+        echo "<li>".$post->title."</li>";
     }
     echo "</ul>";
-
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
-
